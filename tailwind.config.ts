9967cbe0c53ab7f76ff89d816a57d7f191a794ea
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2024-11-17 19:46:45 -0300

    feat: add custom fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        display: ['var(--font-inter)']
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
